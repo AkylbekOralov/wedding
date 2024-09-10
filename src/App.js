5cbea1edf3bdaebb41f5eb89ec7514140afab579
oralovv <oralov26@gmail.com>
2024-09-10 14:20:20 +0500

    Demo Section1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import backr from "./Assets/backr.jpg";
+import "./Style/first.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="section1">
+        <img src={backr} />
+
+        <div class="header-wrapper">
+          <h1>Мадина</h1>
+          <h2>Қыз Ұзату</h2>
+        </div>
+
+        {/* <div class="audio-player">
+            <audio id="audio" src="files/Ainalayin.m4a"></audio>
+            <button type="button" id="play-pause">
+              Әуенді Қосу
+            </button>
+          </div>
+        </div>
+
+        <div class="timer">
+          <div class="nums" id="timer">
+            32:21:49:45
+          </div>
+          <div class="timer-text">
+            <div class="days">дней</div>
+            <div class="hours">часов</div>
+            <div class="minutes">минут</div>
+            <div class="seconds">секунд</div>
+          </div>
+        </div> */}
+      </section>
     </div>
   );
 }
